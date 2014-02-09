17def5e53ecd659b08b2a8aacbc81a22bd236d95
Benjamin T Jones <btjones16@gmail.com>
2014-02-09 17:34:52 -0500

    Added method to create new FVCOM grid

--- R/fvcom_grid.R
+++ R/fvcom_grid.R
@@ -1,7 +1,9 @@
 #' Finite Volume Community Ocean Model grid
 #'
 #' \code{fvcom.grid} provides a represenation of the unstructured triangular
-#' sigma grid used by the Finite Volume Community Ocean Model.
+#' sigma grid used by the Finite Volume Community Ocean Model (FVCOM). Please
+#' note that the author of this package is a user of FVCOM, but is not
+#' affiliated with its development.
 #' 
 #' @section Slots:
 #'   \describe{
@@ -21,9 +23,7 @@ setClass("fvcom.grid",
              nodes.n="integer",
              nodes.x="numeric",
              nodes.y="numeric",
-             nodes.z="numeric",
-             siglay="numeric",
-             siglev="numeric",
+             nodes.h="numeric",
              elems.n="integer",
              elems.v1="integer",
              elems.v2="integer",
@@ -31,26 +31,52 @@ setClass("fvcom.grid",
              )
          )
 
+#' Load an FVCOM grid from a NetCDF output file.
+#'
+#' Loads enough of an FVCOM grid to use the other methods associated with
+#' the \code{fvcom.grid} class. The (x,y,h) locations of the nodes and their
+#' connections to form an unstructured triangular mesh are loaded.
+#' @param filename The name of an output NetCDF file from FVCOM 2.7.
+#' @return An instance of the \code{fvcom.grid} class.
+loadFVCOMGrid27 <- function(filename) {
+    ncid <- nc_open(filename)
+    x <- as.vector(ncvar_get(ncid, 'x'))
+    y <- as.vector(ncvar_get(ncid, 'y'))
+    h <- as.vector(ncvar_get(ncid, 'h'))
+    ev <- ncvar_get(ncid, 'nv') ## Element vertices
+    nc_close(ncid)
+    return(new("fvcom.grid",
+               nodes.n=length(x), nodes.x=y, nodes.y=y, nodes.h=h,
+               elems.n=nrow(ev),
+               elems.v1=ev[,1], elems.v2=ev[,2], elems.v3=ev[,3]))
+}
+
 validFVCOMGrid <- function(object) {
     if((length(object@nodes.x) != object@nodes.n) ||
        (length(object@nodes.y) != object@nodes.n) ||
-       (length(object@nodes.z) != object@nodes.n))
+       (length(object@nodes.h) != object@nodes.n))
        return("Number of x, y, and z coordinates of nodes must be equal.")
     else if((length(object@elems.v1) != object@elems.n) ||
             (length(object@elems.v2) != object@elems.n) ||
             (length(object@elems.v3) != object@elems.n))
         return("All elements must have 3 vertices.")
-    else if((sum(is.inf(c(object@nodes.x, object@nodes.y, object@nodes.z)))
+    else if((sum((c(object@nodes.x, object@nodes.y, object@nodes.h) == Inf))
              != 0 ||
-             sum(is.na(c(object@nodes.x, object@nodes.y, object@nodes.z)))
+             sum(is.na(c(object@nodes.x, object@nodes.y, object@nodes.h)))
              != 0))
         return("Nodes may not have Inf or NA coordinates")
-    else if((sum(is.inf(c(object@nodes.x, object@nodes.y, object@nodes.z)))
+    else if((sum((c(object@nodes.x, object@nodes.y, object@nodes.h) == Inf))
              != 0 ||
-             sum(is.na(c(object@nodes.x, object@nodes.y, object@nodes.z)))
+             sum(is.na(c(object@nodes.x, object@nodes.y, object@nodes.h)))
              != 0))
         return("Elems may not have Inf or NA indices")
     else return(TRUE)
 }
 setValidity("fvcom.grid", validFVCOMGrid)
 
+#' Create a new FVCOM grid instance from a FVCOM NetCDF output file.
+#' 
+#' @param filename The name of an output NetCDF file from FVCOM.
+#' @return An instance of the \code{fvcom.grid} class.
+fvcom.grid <- function(filename)
+    return(loadFVCOMGrid27(filename))
