014a5ff558f70afab10cabde921b40b21b23aaa4
Benjamin T Jones <btjones16@gmail.com>
2014-05-20 17:14:13 -0400

    Updated.

--- tests/test_fvcom_grid.R
+++ tests/test_fvcom_grid.R
@@ -1,7 +1,14 @@
 #!/usr/local/bin/Rscript
 
 ## This file implements a suite of unittests for the FVCOM grid class.
-
 library(testthat)
+
+## The FVCOM grid context tests the constructor for the \code{fvcom.grid}
+## class.
+context('FVCOM grid')
+test_that('the FVCOM grid is correctly loaded from a FVCOM output file.',
+          {
+              
+          })
 
 
